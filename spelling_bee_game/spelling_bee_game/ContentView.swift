b89cf1929fa39a608a7984770aaa864baf749696
Roy Schor <rps5836@psu.edu>
2024-01-15 17:53:20 -0500

    new project initial commit

---
+++ spelling_bee_game/spelling_bee_game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spelling_bee_game
+//
+//  Created by Roy Schor on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spelling_bee_game/spelling_bee_game/spelling_bee_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  spelling_bee_gameApp.swift
+//  spelling_bee_game
+//
+//  Created by Roy Schor on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct spelling_bee_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
