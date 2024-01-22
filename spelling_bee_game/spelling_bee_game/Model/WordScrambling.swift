dc4c8e7642e98854d8871d9a93a5effafae68a5c
Roy Schor <rps5836@psu.edu>
2024-01-22 13:48:41 -0500

    Added model

---
+++ spelling_bee_game/spelling_bee_game/Model/WordScrambling.swift
@@ -0,0 +1,26 @@
+//
+//  WordScrambling.swift
+//  spelling_bee_game
+//
+//  Created by Roy Schor on 1/22/24.
+//
+
+import Foundation
+
+struct ScrambleGame {
+    var numLetters: Int
+    var letters: [String]
+    var foundWords: [String]
+    var currentWord: [String]
+    var score: Int
+    
+    init(numLetters: Int, letters: [String]) {
+        self.numLetters = numLetters
+        self.letters = letters
+        self.foundWords = []
+        self.currentWord = []
+        self.score = 0
+    }
+}
+
+let testGame = ScrambleGame(numLetters: 5, letters: ["a", "t", "s", "p", "n"])
---
+++ spelling_bee_game/spelling_bee_game/ViewModel/GameManager.swift
@@ -0,0 +1,8 @@
+//
+//  GameManager.swift
+//  spelling_bee_game
+//
+//  Created by Roy Schor on 1/22/24.
+//
+
+import Foundation
