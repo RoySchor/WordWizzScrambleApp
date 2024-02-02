e89c360994a75430b26a101f6fddeb94552af434
Roy Schor <70181314+RoySchor@users.noreply.github.com>
2024-02-02 17:03:07 -0500

    Update ActionButtonsView.swift

--- WordWizz/WordWizz/Views/ActionButtonsView.swift
+++ WordWizz/WordWizz/Views/ActionButtonsView.swift
@@ -25,7 +25,7 @@ struct ActionButtonsView: View {
                     .frame(width: 80, height: 55)
                     .foregroundColor(.primary)
                     .background(gameManager.isWordValid ? Color.green.gradient : Color(Constants.FoundWordsBackgroundColorName.bkColor).gradient)
-                    .opacity((gameManager.currentWord.count < 4 || !gameManager.isWordValid) ? 0.4 : 1)
+                    .opacity((gameManager.currentWord.count < 3 || !gameManager.isWordValid) ? 0.4 : 1)
                     .cornerRadius(15)
             }
             .disabled(!gameManager.isWordValid)
